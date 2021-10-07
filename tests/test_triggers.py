21678befc02cfdabebf6043c1c06b1c7070e65b9
hlYassine <hlioui.yassine@gmail.com>
2021-10-08 00:18:49 +0200

    Add get_triggers() unittests

--- tests/test_triggers.py
+++ tests/test_triggers.py
@@ -1,4 +1,39 @@
+import pytest
+
 from perforce_triggers import triggers
+from perforce_triggers import exceptions
+
+
+trigger_script_data = {
+    "name": "trigger_1",
+    "type": "script",
+    "event": "change-commit",
+    "include_paths": ["//path/..."],
+    "command": "python3 %//trigger/main.py%",
+}
+
+command_script_data = {
+    "name": "trigger_2",
+    "type": "command",
+    "event": "pre-user-move",
+    "command": "python3 %//trigger/main_2.py%",
+}
+
+test_script_trigger = triggers.ScriptTrigger(
+    "trigger_1",
+    "change-commit",
+    ["//path/..."],
+    "python3 %//trigger/main.py%"
+)
+
+test_command_trigger = triggers.CommandTrigger(
+    "trigger_2",
+     "pre-user-move",
+      "python3 %//trigger/main_2.py%"
+
+)
+raise_exception = True
+no_exception = False
 
 
 class TestScriptTrigger:
@@ -94,3 +129,40 @@ class TestCommandTrigger:
 
         res = test_command_trigger.get_p4_trigger_lines()
         assert expected == res
+
+
+@pytest.mark.parametrize(
+    "trigger_config, expected, raise_exception",
+    [
+        (
+            [trigger_script_data],
+            [test_script_trigger],
+            no_exception
+        ),
+        (
+            [ trigger_script_data, command_script_data],
+            [test_script_trigger, test_command_trigger],
+            no_exception
+        ),
+        (
+            [
+                {
+                    "name": "trigger_1",
+                    "type": "something"
+                }
+            ],
+            [],
+            raise_exception
+        ),
+    ],
+)
+def test_get_triggers(trigger_config, expected, raise_exception, mocker):
+    mocker.patch("perforce_triggers.config.get_config", return_value={
+            "triggers": trigger_config
+        })
+    if raise_exception:
+        with pytest.raises(exceptions.PerforceTriggersError):
+            triggers.get_triggers()
+    else:
+        res = triggers.get_triggers()
+        assert all(trigger_ in expected for trigger_ in res)
